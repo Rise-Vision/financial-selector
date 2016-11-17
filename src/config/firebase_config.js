a19565812625dc61af12bf49cce71860350d52d7
rodrigopavezi <rodrigopavezi@gmail.com>
2016-11-17 14:35:56 -0200

    Updated firebase project settings

--- src/config/firebase_config.js
+++ src/config/firebase_config.js
@@ -1,8 +1,8 @@
 /*global module:true*/
 
 module.exports = {
-  apiKey: "AIzaSyBzhb4yOdhLNSraacabkbmlGDlcEgycKJE",
-  authDomain: "financial-selector-prototype.firebaseapp.com",
-  databaseURL: "https://financial-selector-prototype.firebaseio.com",
+  apiKey: "AIzaSyA8VXZwqhHx4qEtV5BcBNe41r7Ra0ZThfY",
+  authDomain: "fir-b3915.firebaseapp.com",
+  databaseURL: "https://fir-b3915.firebaseio.com",
   storageBucket: "",
 };
