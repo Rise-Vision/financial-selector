270eac42b163dd72896b73c140c6dad1fca915a1
Tyler Johnson <mail@tyler-johnson.ca>
2016-11-28 13:19:32 -0500

    Add e2e test for sign in

---
+++ test/e2e/google-sign-in.js
@@ -0,0 +1,35 @@
+const assert = require("assert");
+
+describe("Sign in", function() {
+  this.timeout(50000);
+
+  it("should allow sign in with google", ()=>{
+    browser.url("/");
+    let mainWindow = browser.windowHandles().value[0];
+    let additionalChallengeButtonCount = 3;
+
+    browser.element("#google-signin").click();
+    browser.waitUntil(()=>browser.windowHandles().value.length === 2, 5000, "login window not present");
+    browser.window(browser.windowHandles().value[1]);
+
+    browser.waitForExist("#Email");
+    browser.element("#Email").setValue("jenkins@risevision.com");
+    browser.element("#next").click();
+
+    browser.waitForVisible("#Passwd");
+    browser.element("#Passwd").setValue(process.env.JENKINSPASS);
+    browser.element("#signIn").click();
+
+    browser.waitUntil(()=>{
+      if (browser.windowHandles().value.length === 1) {return true;}
+      if (browser.elements("button").value.length === additionalChallengeButtonCount) {
+        browser.elements("button").value[1].click();
+        browser.element("#answer").setValue("toronto");
+        browser.element("#submit").click();
+      }
+    }, 15000, "could not complete signin process");
+
+    browser.window(mainWindow);
+    browser.waitForExist("#add-display", 15000);
+  });
+});
