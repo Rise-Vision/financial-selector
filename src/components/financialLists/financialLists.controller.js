a04dad2c98bce6726879573435a47841bfc89786
Tyler Johnson <mail@tyler-johnson.ca>
2016-12-15 13:29:52 -0500

    Introduce Add List UI functionality

--- src/components/financialLists/financialLists.controller.js
+++ src/components/financialLists/financialLists.controller.js
@@ -2,6 +2,15 @@ class FinancialListsController {
   constructor() {
     this.name = "financialLists";
   }
+
+  addList() {
+    console.log("adding " + this.newListName);
+  }
+
+  cancel() {
+    this.showAddList = false;
+    this.newListName = "";
+  }
 }
 
 export default FinancialListsController;
