613d06fb33d29dbba1e6ad8f8b7516511baa7ad8
Xiyang Chen <settinghead@gmail.com>
2017-01-02 11:30:00 -0500

    fix add display e2e test

--- test/e2e/add-display.js
+++ test/e2e/add-display.js
@@ -5,7 +5,8 @@ const assert = require( "assert" ),
   serviceAccount = require( path.join( __dirname, "private-keys", "financial-selector", "financial-selector-stage-ee22d821c41a-service-account-e2e-tests.json" ) ),
   jenkinsCreds = require( path.join( __dirname, "private-keys", "financial-selector", "jenkins-creds.json" ) ),
   e2eUserPath = "users/kO4s1ZzjRIP7JVhBSjQ3Etgszpm2",
-  e2eHelper = require( "rv-common-e2e" ).helper;
+  e2eHelper = require( "rv-common-e2e" ).helper,
+  testDisplayId = "G6B7ET3EHGD4";
 
 firebase.initializeApp( {
   credential: firebase.credential.cert( serviceAccount ),
@@ -26,17 +27,17 @@ describe( "Add Display", function() {
 
     e2eHelper.googleSignIn( jenkinsCreds.email, jenkinsCreds.pass, "toronto" );
 
-    browser.waitForVisible( "button=Add Display" )
-    browser.click( "button=Add Display" );
+    browser.waitForVisible( "#add-display-button" )
+    browser.click( "#add-display-button" );
     browser.waitForVisible( "#display-id" )
     browser.element( "#display-id" ).setValue( "1" );
     browser.waitForVisible( "#error-message", 8000 );
-    browser.element( "#display-id" ).setValue( "G6B7ET3EHGD4" );
+    browser.element( "#display-id" ).setValue( testDisplayId );
     browser.waitForVisible( "#error-message", 9000, "inverse" )
     browser.pause( 500 );
     browser.click( "#submit-display-id" );
-    browser.waitForVisible( "button=Add Display", 5000 );
+    browser.waitForVisible( "#add-display-button", 5000 );
     browser.waitForText( "tbody td", 10000 );
-    assert.equal( browser.getText( "tbody :last-child td strong" ), expectedAddedDisplayName );
+    assert.equal( browser.getText( `strong=${expectedAddedDisplayName}` ), expectedAddedDisplayName );
   } );
 } );
