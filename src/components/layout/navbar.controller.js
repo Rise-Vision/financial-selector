56360f469bc8da7f1773561597348062ebac9e02
Tyler Johnson <mail@tyler-johnson.ca>
2017-02-02 15:49:07 -0500

    Clear username on logout

--- src/components/layout/navbar.controller.js
+++ src/components/layout/navbar.controller.js
@@ -2,31 +2,19 @@ class NavbarController {
   constructor( $scope, $state, authService ) {
     "ngInject";
 
-    const ctrl = $scope.$ctrl = this;
-
-    ctrl.name = "navbar";
-
-    ctrl.logout = logout;
-    ctrl.user = null;
+    this.logout = logout;
+    this.user = null;
 
     authService.firebaseAuthObject.$onAuthStateChanged( ( user ) => {
+      this.user = user;
       if ( user ) {
-        _populateUser();
+        this.user.name = user.displayName;
       }
     } );
 
     function logout() {
       authService.logout();
       $state.go( $state.$current, null, { reload: true } );
-    }
-
-    async function _populateUser() {
-      let { displayName, email } = await authService.getAuth();
-
-      ctrl.user = {
-        name: displayName,
-        email: email
-      };
     }
   }
 }
