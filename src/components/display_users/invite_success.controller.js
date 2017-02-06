9e63e440b7cd67a4b09d52668a4acc6edcecd364
Tyler Johnson <mail@tyler-johnson.ca>
2017-02-06 11:48:23 -0500

    Add mailto link to invite user success modal

--- src/components/display_users/invite_success.controller.js
+++ src/components/display_users/invite_success.controller.js
@@ -15,6 +15,8 @@ class inviteSuccessController {
     let _populateInvite = $async( async () => {
       try {
         ctrl.email = decodeForFirebaseProp( ctrl.userId );
+        ctrl.mailTo = `mailto:${ctrl.userId}?subject=${encodeURIComponent( "Rise Financial Selector - I've added you as a User for Display " + ctrl.displayId )}&body=${encodeURIComponent( "You can now manage the Financial Instruments for the " + ctrl.displayId + " Display. Just sign in at https://selector.risevision.com using USERID as your username.\n\nIf USERID is a Google Account just select Log in with Google.\n\nIf USERID is not a Google Account you'll need to sign up and create your password first if you haven't already. You can do that here, https://selector.risevision.com/signup.\n\nThanks,\n\n" )}`;
+        ctrl.mailTo = ctrl.mailTo.replace( /USERID/g, ctrl.userId );
       } catch ( e ) {
         ctrl.errorMessage = `failed to load invite: ${e.message}`;
         console.error( e );
