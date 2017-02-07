2d2ab347d552647b38f185ac92d18422bdbc800d
Tyler Johnson <mail@tyler-johnson.ca>
2017-02-07 11:59:19 -0500

    Update message with display name vs display id

--- src/components/display_users/invite_success.controller.js
+++ src/components/display_users/invite_success.controller.js
@@ -14,9 +14,11 @@ class inviteSuccessController {
 
     let _populateInvite = $async( async () => {
       try {
+        let displayObj = await displayUsersService.getDisplay( ctrl.displayId );
+
+        ctrl.displayName = displayObj.displayName;
         ctrl.email = decodeForFirebaseProp( ctrl.userId );
-        ctrl.mailTo = `mailto:${ctrl.userId}?subject=${encodeURIComponent( "Rise Financial Selector - I've added you as a User for Display " + ctrl.displayId )}&body=${encodeURIComponent( "You can now manage the Financial Instruments for the " + ctrl.displayId + " Display. Just sign in at https://selector.risevision.com using USERID as your username.\n\nIf USERID is a Google Account just select Log in with Google.\n\nIf USERID is not a Google Account you'll need to sign up and create your password first if you haven't already. You can do that here, https://selector.risevision.com/signup.\n\nThanks,\n\n" )}`;
-        ctrl.mailTo = ctrl.mailTo.replace( /USERID/g, ctrl.userId );
+        ctrl.mailTo = `mailto:${ctrl.userId}?target=_blank&subject=${encodeURIComponent( "Rise Financial Selector - I've added you as a User for Display " + ctrl.displayName )}&body=${encodeURIComponent( "I've added you as a User to the Rise Vision Financial Selector. With this you can easily control which stocks are shown on " + ctrl.displayName + " Display.\n\nYour username is " + ctrl.email + ". You can use Google Authentication as your password. If you don't want to use Google Authentication, or " + ctrl.email + " is not a Google Account, you can create your own password at https://selector.risevision.com/signup.\n\nThanks,\n\n" )}`;
       } catch ( e ) {
         ctrl.errorMessage = `failed to load invite: ${e.message}`;
         console.error( e );
